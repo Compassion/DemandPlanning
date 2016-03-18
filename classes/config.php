65bf22a7d0447978e05e49162056b1f3075d1a7a
Josh Wayman <nowayman121@gmail.com>
2016-03-19 09:42:57 +1100

    Refactored config file.
    How I had set it up was annoying me. Subtle changes but much nicer code.

--- classes/config.php
+++ classes/config.php
@@ -1,33 +1,41 @@
 <?php
 /*Configuration Settings*/
-
+/*
+    Set the environment.
+*/
 define('ENVIRONMENT', 'test');
+
 /*
     Set your partner id.
 */
 define('PARTNER_ID', 'AU');
 
-
+/*
+    Set your API key.
+*/
 define('API_KEY', 'this-is-a-key');
-    
-$test = 'https://api2.compassion.com/test/ci/v2/am/demandplanning?api_key=' .API_KEY;
-$staging = 'https://api2.compassion.com/stage/ci/v2/am/demandplanning?api_key=' .API_KEY;
-$production = 'https://api2.compassion.com/ci/v2/am/demandplanning?api_key=' .API_KEY;
 
 
-switch(ENVIRONMENT) {
-        
-    case 'production' :
-        define('REST_URL', $production);
-        break;
-        
-    case 'staging' :
-        define('REST_URL', $staging);
-        break;
-        
-    default :
-        define('REST_URL', $test);
+/* 
+    Build URLs  based on key and environment.
+    Depending on future developments may need to make the endpoint URLs to be part of the config. That way this thing could push the data anywhere where it is needed.
+*/
+switch(ENVIRONMENT) {  
+case 'production' :
+    /* Production */
+    $url = 'https://api2.compassion.com/ci/v2/am/demandplanning?api_key=' .API_KEY;
+    break;
 
-    }
-    
+case 'staging' :
+    /* Staging */
+    $url = 'https://api2.compassion.com/stage/ci/v2/am/demandplanning?api_key=' .API_KEY;
+    break;
+
+default :
+    /* TEST */
+    $url = 'https://api2.compassion.com/test/ci/v2/am/demandplanning?api_key=' .API_KEY;;
+
+}
+
+define('REST_URL', $url);
 ?>
